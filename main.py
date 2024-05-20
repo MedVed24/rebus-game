e1ff533af3ba3b05b84d0313bb99fb50bf6f1569
MedVed <med241208@gmail.com>
2024-05-20 19:03:37 +0700

    Обновил проект

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from tkinter.messagebox import showerror, showwarning, showinfo
 import random
 import time
 from PIL import Image, ImageTk
 import game_points
 import tips_logic
 from textwrap import wrap
+#import test_read
 
 num_level = 0 #Номер уровня
 answer = ''
@@ -46,6 +48,8 @@ def fun_start(event):
             but_x[i] = 50 * i + 100
             but_y[i] = 480
             but[i].place(x=new_x[i], y=480)
+        for i in range(count_b[num_level], count_but):
+            but[i].place_forget()
         #ent.delete(0, END)
 
     def new_level():
@@ -57,8 +61,9 @@ def fun_start(event):
         label1['text'] = ""
 #        ent.delete(0, END)
         if num_level > len(list_answer) - 1: # Завершение игры
-            label1['text'] = 'Игра окончена'
-            game.update()
+#           label1['text'] = 'Игра окончена'
+            showinfo(title="Завершение игры", message="Игра окончена")
+#            game.update()
             time.sleep(3)
             return
 
@@ -68,13 +73,17 @@ def fun_start(event):
         photo[2]['image'] = list_img[num_level][2]
         photo[3]['image'] = list_img[num_level][3]
         for i in range(count_but):
-            but[i]['text'] = list_letters[num_level][i]
-            new_x[num_but] = 50 * i + 100
-            but_x[num_but] = 50 * i + 100
-            but_y[num_but] = 480
-            but[i].place(x=new_x[num_but], y=480)
+            but[i]['text'] = ''
+            new_x[i] = 50 * i + 100
+            but_x[i] = 50 * i + 100
+            but_y[i] = 480
+            but[i].place(x=new_x[i], y=480)
         #    game.itemconfig(but[i], text=list_letters[num_level][i])
+        for i in range(count_b[num_level]):
+            but[i]['text'] = list_letters[num_level][i]
         answer = ""
+        for i in range(count_b[num_level], count_but):
+            but[i].place_forget()
 
     game = Toplevel()
     game.geometry('800x600+0+50')
@@ -83,10 +92,10 @@ def fun_start(event):
     num_but = 0
     abc = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
     list_letters = []
-    count_but = 13
+    count_but = 15
 
     def update_tips_text(text):
-        label1['text']=text
+        label1['text'] = text
 
     def update_points_text(points):
         label_points['text'] = points
@@ -99,10 +108,10 @@ def fun_start(event):
         label_points=lambda points: update_points_text(points)
     ))
 
-
     for i in range(len(list_answer)):
         ansi = list_answer[i]
-        for j in range(count_but - len(list_answer[i])):
+        count_but0 = count_b[i]
+        for j in range(count_but0 - len(list_answer[i])):
             ansi = ansi + abc[random.randint(0, len(abc) - 1)]
         ## Преобразуем строку в список
         list_ansi = list(ansi)
@@ -110,6 +119,7 @@ def fun_start(event):
         random.shuffle(list_ansi)
         list_letters.append(list_ansi)
     print(list_letters)
+    count_but = 15
 
     label1 = Label(game, font=("Arial", 30))
     label1.place(x=100, y=50)
@@ -132,18 +142,16 @@ def fun_start(event):
     photo[3].place(x=300, y=300)
 
     label_points = Label(game, text=game_points.__points, font=("Arial", 20))
-    label_points.place(x=100, y=10)
+    label_points.place(x=200, y=20)
 
     label_p = Label(game, text="Баллы:", font=("Arial", 20))
-    label_p.place(x=0, y=10)
-
-
+    label_p.place(x=100, y=20)
 
     ## Создание набора кнопок
     old_x = [0 for i in range(count_but)]
     new_x = [0 for i in range(count_but)]
     for i in range(0, count_but):
-        but.append(Button(game, text=list_letters[0][i], width=4, height=2))
+        but.append(Button(game, text='', width=4, height=2))
         but[i].bind('<ButtonPress>', lambda event, par1=i: f_k(event, par1))
         but_x.append(0)
         but_y.append(0)
@@ -151,8 +159,13 @@ def fun_start(event):
         old_x[i] = 50*i + 100
         but_y[i] = 480
         but[i].place(x=but_x[i], y=but_y[i])
+    for i in range(0, count_b[num_level]):
+        but[i]['text'] = list_letters[num_level][i]
 
-    del_b = Button(game, text='Очистить', width=10, height=2)
+    for i in range(count_b[num_level], count_but):
+        but[i].place_forget()
+
+    del_b = Button(game, text='Очистить', width=16, height=2, font='Arial 12')
     del_b.place(x=300, y=20)
     del_b.bind('<ButtonPress>', delete_t)
 
@@ -165,40 +178,38 @@ def fun_desc(event):
     descr = Tk()
     descr.geometry('800x600+0+50')
     descr.title("Описание игры")
-    text = "Это очень увлекательная и затягивающая игра, в которой нужно угадывать слова по "\
+    text_descr = "Это очень увлекательная и затягивающая игра, в которой нужно угадывать слова по "\
     "четырем картинкам, связанные с ними. Некоторые уровни очень сложные и заставят вас поломать голову.\n"\
-    "Игра подходит для любителей кроссвордов, сканвордов, загадок, ребусов и логических игр. Кроме того,"\
-    "игра помогает развивать мозг, память и внимание."
-
-    lbl_desc = Label(descr, text=text, font=("Georgia", 30))
-    lbl_desc.grid(row=0, column=0)
+    "Игра подходит для любителей кроссвордов, сканвордов, загадок, ребусов и логических игр.\n"\
+    "Кроме того, игра помогает развивать мозг, память и внимание."
+    text1 = Text(descr, height=25, width=70, padx=20, font='Georgia 30', wrap=WORD)
+    text1.insert(1.0, text_descr)
+    text1.pack()
     descr.update()
-    # if lbl_desc.winfo_width() > descr.winfo_width():
-    #     # Вычисляем среднюю ширину символа
-    #     average_char_width = lbl_desc.winfo_width() / len(text)
-    #     # Приблизительно рассчитываем количество символов, которое помещается в окне
-    #     chars_per_line = int(descr.winfo_width() / average_char_width)
-    #     # В цикле уменьшаем это количество, пока текст не станет помещаться
-    #     while lbl_desc.winfo_width() > descr.winfo_width():
-    #         wrapped_text = '\n'.join(wrap(text, chars_per_line))
-    #         lbl_desc['text'] = wrapped_text
-    #         descr.update()
-    #         chars_per_line -= 1
-
-
 
 def fun_opt(event):
     option = Tk()
     option.geometry('800x600+0+50')
     option.title("Настройка игры")
 
-menu = Tk()
-menu.geometry('790x700')
+menu=Tk()
+menu.geometry('790x700+0+50')
 menu.title("4 фото одно слово")
 list_img = []
 
-with open("answers2.txt", "r", encoding="utf8") as file:
-    list_answer = file.read().splitlines()
+#with open("answers2.txt", "r", encoding="utf8") as file:
+#    list_answer = file.read().splitlines()
+
+#with open("count_b.txt", "r", encoding="utf8") as file:
+#    count_but = file.read().splitlines()
+file = open('answers.txt', 'r', encoding="utf8")
+list_answer = []
+count_b = []
+for line in file:
+    a, b = line.split()
+    list_answer.append(a)
+    count_b.append(int(b))
+
 
 #file = open('answers.txt', 'r')
 #list_answer = file.readlines()
@@ -215,16 +226,16 @@ imgm = ImageTk.PhotoImage(file='menuimg.jpeg')
 lblm = Label(menu, image=imgm, width=790, height=770)
 lblm.pack()
 
-start = Button(menu, width = 40, text = 'Старт')
-start.place(x = 420, y = 280)
+start = Button(menu, width=40, height=3, text='Старт')
+start.place(x=250, y=430)
 start.bind('<ButtonPress>', fun_start)
 
-desc = Button(menu, width = 40, text = 'Описание') ##Описание кнопки описания
-desc.place(x=420, y=315)
+desc = Button(menu, width=40, height=3, text='Описание') ##Описание кнопки описания
+desc.place(x=250, y=490)
 desc.bind('<ButtonPress>', fun_desc)
 
-opt = Button(menu, width = 40, text = 'Настройки') ##Описание кнопки настройки
-opt.place(x=420, y=350)
+opt = Button(menu, width=40, height=3, text='Настройки') ##Описание кнопки настройки
+opt.place(x=250, y=550)
 opt.bind('<ButtonPress>', fun_opt)
 
 menu.mainloop()
