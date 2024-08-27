90aae56ed4d6ad9b892a333aba78a10600c18258
MedVed <med241208@gmail.com>
2024-08-27 13:44:04 +0700

    Вроде конечный вариант

--- tips_logic.py
+++ tips_logic.py
@@ -1,10 +1,9 @@
 import game_points
-def on_tips_button_click(event, num_tips_click, len_answer, label_text, label_points, show_tips, fun_shop):
+def on_tips_button_click(event, num_tips_click, len_answer, label_text, label_points, show_tips):
     if num_tips_click<len_answer:
         if game_points.__points <= 0:
             label_text("У вас нет баллов!")
             return
         show_tips()
-        fun_shop()
         game_points.points_down()
         label_points(game_points.__points)
